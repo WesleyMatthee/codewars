20073d63371fee9468e51ceb1697e284bed9804d
Wesley Matthee <wrmatthee@gmail.com>
2023-04-24 16:28:45 -0400

    without strings refactor

--- 7kyu/withoutStrings.js
+++ 7kyu/withoutStrings.js
@@ -8,5 +8,19 @@ You cannot use the following:
 `Hello, World!`
 */
 const helloWorld = () => {
-	return String('Hello, World!');
+	return String.fromCharCode(
+		72,
+		101,
+		108,
+		108,
+		111,
+		44,
+		32,
+		87,
+		111,
+		114,
+		108,
+		100,
+		33
+	);
 };
