b4a1e6e09e85b5c1391107524bc92bd310f2d401
Wesley Matthee <wrmatthee@gmail.com>
2023-05-01 14:29:37 -0400

    Valid Parenthese

---
+++ 7kyu/validParenthese.js
@@ -0,0 +1,34 @@
+/*
+Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+
+Examples
+"()"              =>  true
+")(()))"          =>  false
+"("               =>  false
+"(())((()())())"  =>  true
+Constraints
+0 <= length of input <= 100
+
+All inputs will be strings, consisting only of characters ( and ).
+Empty strings are considered balanced (and therefore valid), and will be tested.
+For languages with mutable strings, the inputs should not be mutated.
+*/
+
+function validParentheses(parenStr) {
+	let numBuffer = 0;
+	for (let i = 0; i < parenStr.length; i++) {
+		if (parenStr.charAt(i) === '(') {
+			numBuffer += 1;
+		} else {
+			numBuffer -= 1;
+		}
+		if (numBuffer < 0) {
+			return false;
+		}
+	}
+	if (numBuffer === 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
--- 8kyu/invertValues.js
+++ 8kyu/invertValues.js
@@ -10,3 +10,4 @@ You can assume that all values are integers. Do not mutate the input array/list.
 function invert(array) {
 	return array.map(num => num * -1);
 }
+
