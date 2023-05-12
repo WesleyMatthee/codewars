d9aa302bce64e3581c29233236c82c50cecfebff
Wesley Matthee <wrmatthee@gmail.com>
2023-05-12 15:52:28 -0400

    Greeting

---
+++ 4kyu/getPins.js
@@ -0,0 +1,21 @@
+function getPINs(observed) {
+	let pinOpt = [];
+	let adjNums = {
+		0: ['8'],
+		1: ['2', '4'],
+		2: ['1', '3', '5'],
+		3: ['2', '6'],
+		4: ['1', '5', '7'],
+		5: ['2', '4', '6', '8'],
+		6: ['3', '5', '9'],
+		7: ['4', '8'],
+		8: ['5', '7', '9', '0'],
+		9: ['6', '8'],
+	};
+
+	let strNums = observed.toString().split(' ');
+  
+  
+
+	// TODO: This is your job, detective!
+}
