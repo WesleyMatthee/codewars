6614af793c2c8772f39a31ebb27c3daa4abcf7e9
Wesley Matthee <wrmatthee@gmail.com>
2023-09-14 15:03:07 -0400

    Add characteConcat function

---
+++ 7kyu/characterConcat.js
@@ -0,0 +1,26 @@
+/*
+Given a string, you progressively need to concatenate the first letter from the left and the first letter to the right and "1", then the second letter from the left and the second letter to the right and "2", and so on.
+
+If the string's length is odd drop the central element.
+
+For example:
+
+charConcat("abcdef")    == 'af1be2cd3'
+charConcat("abc!def")   == 'af1be2cd3' // same result
+*/
+
+function charConcat(string) {
+	let str = string.split('');
+	let arr = [];
+
+	for (let i = 1; i <= str.length / 2; i++) {
+		//This loop is designed to process characters from both ends of the input string, moving towards the center.
+
+		arr.push(str[i - 1]);
+		arr.push(str[str.length - i]);
+		arr.push(i);
+	}
+	return arr.join('');
+}
+
+charConcat('Wesley');
