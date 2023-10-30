27a55bc4fc804054153013c65b9d1ce7a367e53d
Wesley Matthee <wrmatthee@gmail.com>
2023-10-30 13:51:18 -0400

    7kyu Leap Year Checker Vanilla version added

--- 7kyu/leapYear.js
+++ 7kyu/leapYear.js
@@ -8,3 +8,13 @@ Tested years are in range 1600 ≤ year ≤ 4000. */
 function isLeapYear(year) {
 	return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 }
+
+/*
+// THIS ALSO WORKS AND MIGHT BE EASIER TO READ
+function isLeapYear(year) {
+	if (0 == year % 400) return true;
+	if (0 == year % 100) return false;
+	if (0 == year % 4) return true;
+	return false;
+}
+*/
