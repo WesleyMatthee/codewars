4527d006ce5466ecc15844bb1c33fe50a0993020
Wesley Matthee <wrmatthee@gmail.com>
2024-07-24 13:47:30 -0400

    5kyu Josephus Permutation

---
+++ 5kyu/josephus.js
@@ -0,0 +1,28 @@
+/*You are now to create a function that returns a Josephus permutation, taking as parameters the initial array/list of items to be permuted as if they were in a circle and counted out every k places until none remained.
+
+Tips and notes: it helps to start counting from 1 up to n, instead of the usual range 0 to n-1; k will always be >=1.
+
+For example, with an array=[1,2,3,4,5,6,7] and k=3, the function should act this way.
+
+[1,2,3,4,5,6,7] - initial sequence
+[1,2,4,5,6,7] => 3 is counted out and goes into the result [3]
+[1,2,4,5,7] => 6 is counted out and goes into the result [3,6]
+[1,4,5,7] => 2 is counted out and goes into the result [3,6,2]
+[1,4,5] => 7 is counted out and goes into the result [3,6,2,7]
+[1,4] => 5 is counted out and goes into the result [3,6,2,7,5]
+[4] => 1 is counted out and goes into the result [3,6,2,7,5,1]
+[] => 4 is counted out and goes into the result [3,6,2,7,5,1,4]*/
+
+function josephus(array, k) {
+	const result = [];
+	let index = 0;
+
+	while (array.length > 0) {
+		// Calculate the index of the next element to be removed
+		index = (index + k - 1) % array.length;
+		// Remove the element and add it to the result array
+		result.push(...array.splice(index, 1));
+	}
+
+	return result;
+}
