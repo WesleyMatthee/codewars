d678c609a88d8c925267feecfc578c95dabdaa52
Wesley Matthee <wrmatthee@gmail.com>
2024-07-31 15:53:20 -0400

    The Office III Broken Photocopier

---
+++ 7kyu/theOffice3BrokenPhotocopier.js
@@ -0,0 +1,19 @@
+/*The bloody photocopier is broken... Just as you were sneaking around the office to print off your favourite binary code!
+
+Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
+
+Given a string of binary, return the version the photocopier gives you as a string.
+
+*/
+function broken(x) {
+	let result = [];
+	let arr = x.split('');
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] === '0') {
+			result.push('1');
+		} else {
+			result.push('0');
+		}
+	}
+	return result.join('');
+}
