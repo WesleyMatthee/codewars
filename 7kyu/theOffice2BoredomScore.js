2f078c139f89f2dfaa354db7eeca868ac19e9ae3
Wesley Matthee <wrmatthee@gmail.com>
2024-08-01 10:43:45 -0400

    The Office 2 Boredom Score using Reduce with Example Tests

--- 7kyu/theOffice2BoredomScore.js
+++ 7kyu/theOffice2BoredomScore.js
@@ -56,6 +56,33 @@ function boredom(staff) {
 	return 'party time!!';
 }
 
+//USING REDUCE
+function boredom(staff) {
+	const map = {
+		accounts: 1,
+		finance: 2,
+		canteen: 10,
+		regulation: 3,
+		trading: 6,
+		change: 6,
+		IS: 8,
+		retail: 5,
+		cleaning: 4,
+		'pissing about': 25,
+	};
+
+	let score = Object.keys(staff).reduce(function (a, b) {
+		return a + map[staff[b]];
+	}, 0);
+
+	return score <= 80
+		? 'kill me now'
+		: score < 100 && score > 80
+		? 'i can handle this'
+		: 'party time!!';
+}
+
+
 //TEST
 describe('Example tests', () => {
 	Test.assertEquals(
