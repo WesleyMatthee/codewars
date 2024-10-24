6a594f61320529bfbf7ab02b6fe09c181dc26bf1
Wesley Matthee <wrmatthee@gmail.com>
2024-10-24 16:06:09 -0400

    This function receives a string and parse the assigned letters to math functions Using the reduce method

--- 6kyu/makeDeadFishSwim.js
+++ 6kyu/makeDeadFishSwim.js
@@ -47,3 +47,19 @@ describe("Example test", () => {
   });
 });
 */
+
+//Function using reduce
+function parse(data) {
+	let res = [];
+
+	data.split('').reduce((cur, s) => {
+		if (s === 'i') cur++;
+		if (s === 'd') cur--;
+		if (s === 's') cur = Math.pow(cur, 2);
+		if (s === 'o') res.push(cur);
+
+		return cur;
+	}, 0);
+
+	return res;
+}
